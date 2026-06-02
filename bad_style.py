c04fa01153765b6c9d75a0e8b2df9c6602232331
Zarko Grozdanovic <zarkog@oipinsurtech.com>
2026-06-02 16:39:40 +0200

    Adjusted the code in bad_style.py

--- bad_style.py
+++ bad_style.py
@@ -1,9 +1,11 @@
-def calculate_area ( length , width ):
+def calculate_area(length, width):
     result = length * width
-    if result > 100: print ( "Large area!" )
+    if result > 100:
+        print("Large area!")
     return result
+
 
 my_length = 10
 my_width = 5
-area = calculate_area ( my_length, my_width )
-print ( f"The area is: {area}" )
+area = calculate_area(my_length, my_width)
+print(f"The area is: {area}")
